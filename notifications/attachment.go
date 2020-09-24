cc9c982f49950589a2d5c0d59f2077051c4b3a47
Taliesin Millhouse <taliesinwrmillhouse@gmail.com>
2020-09-24 12:33:44 +1000

    feat: Load attachments in memory
    Updated the package that builds the raw email to a fork of itself. The original package is no longer maintained. This new package support loading attachments into memory, instead of storing in on file temporarily until after the email is sent and then deleting it.
    
    refs: 9204

--- notifications/attachment.go
+++ notifications/attachment.go
@@ -3,16 +3,12 @@ package notifications
 import (
 	"context"
 	"fmt"
-	"io"
+	"io/ioutil"
 	"net/http"
-	"os"
-	"path"
-	"path/filepath"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/gofor-little/rand"
 )
 
 // AttachmentType is a string type alias for the different supported attachment types.
@@ -34,17 +30,16 @@ type Attachment struct {
 	Type AttachmentType `json:"type"`
 }
 
-// Load loads an Attachment's data and stores is locally in /tmp to be used.
-func (a *Attachment) Load(ctx context.Context) (string, error) {
+// Load loads an Attachment's data and returns it as a byte slice.
+func (a *Attachment) Load(ctx context.Context) ([]byte, error) {
 	switch a.Type {
 	case AttachmentTypeLocal:
-		// Just return the local file path.
-		return a.Path, nil
+		return ioutil.ReadFile(a.Path)
 	case AttachmentTypeS3:
 		// Split the path up to get the bucket and key.
 		parts := strings.Split(a.Path, "/")
 		if len(parts) < 2 {
-			return "", fmt.Errorf("invalid path for AttachmentTypeS3: %s, path must be in the following format <bucket>/<key>", a.Path)
+			return nil, fmt.Errorf("invalid path for AttachmentTypeS3: %s, path must be in the following format <bucket>/<key>", a.Path)
 		}
 
 		buffer := aws.NewWriteAtBuffer(nil)
@@ -57,118 +52,24 @@ func (a *Attachment) Load(ctx context.Context) (string, error) {
 		// Download the Attachment data.
 		_, err := s3Downloader.DownloadWithContext(ctx, buffer, input)
 		if err != nil {
-			return "", fmt.Errorf("failed to download attachment from S3: %w", err)
+			return nil, fmt.Errorf("failed to download attachment from S3: %w", err)
 		}
 
-		// Find a free file path for the Attachment file.
-		freePath, err := a.getAvailablePath()
-		if err != nil {
-			return "", fmt.Errorf("failed to get free path: %w", err)
-		}
-
-		// Create the Attachment file in the /tmp directory.
-		file, err := os.Create(freePath)
-		if err != nil {
-			return "", fmt.Errorf("failed to create attachment file: %w", err)
-		}
-		defer func() {
-			_ = file.Close()
-		}()
-
-		// Write the Attachment data to file.
-		_, err = file.Write(buffer.Bytes())
-		if err != nil {
-			return "", fmt.Errorf("failed to write buffer to file: %w", err)
-		}
-
-		return freePath, nil
+		return buffer.Bytes(), nil
 	case AttachmentTypeHTTP:
 		// Get the Attachment data.
 		response, err := http.Get(a.Path)
 		if err != nil {
-			return "", fmt.Errorf("failed to perform HTTP Get request on attachment: %w", err)
+			return nil, fmt.Errorf("failed to perform HTTP Get request on attachment: %w", err)
 		}
 		defer func() {
 			_ = response.Body.Close()
 		}()
 
-		// Find a free file path for the Attachment file.
-		freePath, err := a.getAvailablePath()
-		if err != nil {
-			return "", fmt.Errorf("failed to get free path: %w", err)
-		}
-
-		// Create the Attachment file in the /tmp directory.
-		file, err := os.Create(freePath)
-		if err != nil {
-			return "", fmt.Errorf("failed to create attachment file: %w", err)
-		}
-		defer func() {
-			_ = file.Close()
-		}()
-
-		// Copy the Attachment data to file.
-		_, err = io.Copy(file, response.Body)
-		if err != nil {
-			return "", fmt.Errorf("failed to copy HTTP response body to file: %w", err)
-		}
-
-		return freePath, nil
+		return ioutil.ReadAll(response.Body)
 	default:
 		// Fallthrough to the error case as a.Type is not a supported AttachmentType.
 	}
 
-	return "", fmt.Errorf("unable to load Attachment with unexpected type: %s", a.Type)
-}
-
-// getAvailablePath will ensure the path that the file will be stored in doesn't
-// collide with another file.
-func (a *Attachment) getAvailablePath() (string, error) {
-	// Get the base or file name from the path.
-	base := path.Base(a.Path)
-	// If we don't get a valid file name create one.
-	if base == "." || base == "/" {
-		var err error
-		base, err = rand.GenerateCryptoString(16)
-		if err != nil {
-			return "", err
-		}
-	}
-
-	freePath := fmt.Sprintf("/tmp/%s", base)
-
-	// Check if the path is available, if so return that path.
-	if _, err := os.Stat(freePath); err != nil {
-		if os.IsNotExist(err) {
-			return freePath, nil
-		}
-
-		return "", err
-	}
-
-	index := 0
-	isFreePath := false
-
-	// If the path isn't available append the index to the filename (without the
-	// extension) until an available path is found. For example, file_1.txt, file_2.txt.
-	for !isFreePath {
-		extension := filepath.Ext(freePath)
-		name := freePath[0 : len(freePath)-len(extension)]
-
-		fp := fmt.Sprintf("%s_%d%s", name, index, extension)
-
-		if _, err := os.Stat(fp); err != nil {
-			if os.IsNotExist(err) {
-				isFreePath = true
-				freePath = fp
-				continue
-			}
-
-			return "", err
-		}
-
-		index++
-	}
-
-	return freePath, nil
+	return nil, fmt.Errorf("unable to load Attachment with unexpected type: %s", a.Type)
 }
